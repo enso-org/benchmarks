44c9233c68bb50ba8ca1e4606e3f854b5b4ee02a
Radosław Waśko <wasko.radek@gmail.com>
2021-03-30 23:27:12 +0200

    Python

--- python/main.py
+++ python/main.py
@@ -1,14 +1,15 @@
 import time, os
 
 def print_header():
-    print("language, benchmark, run_id, time")
+    print("language,benchmark,run_id,time")
 
-def measure(act, label, num_iters):
+def measure(act, label, iter_size, num_iters):
     for it_num in range(num_iters):
         x1 = time.perf_counter()
-        act()
+        for _ in range(iter_size):
+            act()
         x2 = time.perf_counter()
-        diff = (x2 - x1)  * 1000.0
+        diff = (x2 - x1)  * 1000.0 / iter_size
         print("python, " + label + ", " + str(it_num) + ", " + str(diff), flush=True)
 
 def sum_bench(n):
@@ -19,13 +20,18 @@ def sum_bench(n):
         i += 1
     return acc
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 def alloc_vector(n):
-    return [i for i in range(n)]
+    return [Point(i, i) for i in range(n)]
 
 def sum_vector(vec):
     acc = 0
-    for i in vec:
-        acc += i
+    for p in vec:
+        acc += p.x + p.y
     return acc
 
 class Cons:
@@ -36,13 +42,14 @@ class Cons:
 def alloc_list(n):
     r = None
     for i in range(n, -1, -1):
-        r = Cons(i, r)
+        r = Cons(Point(i, i), r)
     return r
 
 def sum_list(lst):
     acc = 0
     while lst is not None:
-        acc += lst.head
+        h = lst.head
+        acc += h.x + h.y
         lst = lst.tail
     return acc
 
@@ -60,7 +67,7 @@ def alloc_full_tree(depth):
             return None
         else:
             l = go(remaining_depth - 1)
-            e = ix
+            e = Point(ix, ix)
             ix += 1
             r = go(remaining_depth - 1)
             return Tree(l, e, r)
@@ -70,27 +77,31 @@ def sum_tree(tree):
     if tree is None:
         return 0
     l = sum_tree(tree.left)
+    e = tree.elem
     r = sum_tree(tree.right)
-    return l + tree.elem + r
+    return l + e.x + e.y + r
 
 if __name__ == "__main__":
     print_header()
-    size = int(os.getenv("BENCH_SIZE"))
-    depth = int(os.getenv("BENCH_DEPTH"))
-    n_iters = int(os.getenv("BENCH_ITER"))
+    n_iters = 30
+    iter_size = 50
 
-    measure(lambda: time.sleep(0.1), "100ms", 10)
+    ten_million = 10000000
+    million = 1000000
+    tree_depth = 17
 
-    measure(lambda: sum_bench(size), "sum", n_iters)
+    measure(lambda: time.sleep(0.01), "10ms", iter_size, n_iters)
 
-    measure(lambda: alloc_vector(size), "alloc_vector", n_iters)
-    vec = alloc_vector(size)
-    measure(lambda: sum_vector(vec), "sum_vector", n_iters)
+    measure(lambda: sum_bench(ten_million), "sum", iter_size, n_iters)
 
-    measure(lambda: alloc_list(size), "alloc_list", n_iters)
-    lst = alloc_list(size)
-    measure(lambda: sum_list(lst), "sum_list", n_iters)
+    measure(lambda: alloc_vector(million), "alloc_vector", iter_size, n_iters)
+    vec = alloc_vector(million)
+    measure(lambda: sum_vector(vec), "sum_vector", iter_size, n_iters)
 
-    measure(lambda: alloc_full_tree(depth), "alloc_full_tree", n_iters)
-    tree = alloc_full_tree(depth)
-    measure(lambda: sum_tree(tree), "sum_tree", n_iters)
+    measure(lambda: alloc_list(million), "alloc_list", iter_size, n_iters)
+    lst = alloc_list(million)
+    measure(lambda: sum_list(lst), "sum_list", iter_size, n_iters)
+
+    measure(lambda: alloc_full_tree(tree_depth), "alloc_full_tree", iter_size, n_iters)
+    tree = alloc_full_tree(tree_depth)
+    measure(lambda: sum_tree(tree), "sum_tree", iter_size, n_iters)
