c6bd004fe2a525b1c8e9a03b959f4d013dc02140
Radosław Waśko <wasko.radek@gmail.com>
2021-03-30 23:02:11 +0200

    Fixes

--- enso/microbenchmarks/run.sh
+++ enso/microbenchmarks/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LANG=C
-echo "language;benchmark;run_id;time" > results.csv
+echo "language,benchmark,run_id,time" > results.csv
 enso run src/BenchTiming.enso | tee -a results.csv
 enso run src/BenchSum.enso | tee -a results.csv
 enso run src/BenchAllocVector.enso | tee -a results.csv
