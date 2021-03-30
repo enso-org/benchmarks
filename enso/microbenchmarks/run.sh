afc355f18884613fa073cb4065df7a65b9be7775
Radosław Waśko <wasko.radek@gmail.com>
2021-03-30 22:00:42 +0200

    Implement Enso Benchmarks

--- enso/microbenchmarks/run.sh
+++ enso/microbenchmarks/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+export LANG=C
 echo "language;benchmark;run_id;time" > results.csv
 enso run src/BenchTiming.enso | tee -a results.csv
 enso run src/BenchSum.enso | tee -a results.csv
 enso run src/BenchAllocVector.enso | tee -a results.csv
 enso run src/BenchSumVector.enso | tee -a results.csv
+enso run src/BenchAllocList.enso | tee -a results.csv
+enso run src/BenchSumList.enso | tee -a results.csv
+enso run src/BenchAllocFullTree.enso | tee -a results.csv
+enso run src/BenchSumTree.enso | tee -a results.csv
